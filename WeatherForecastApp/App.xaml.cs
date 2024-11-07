4f2269262b9d564958310c8c4105d21bbd1040d6
Elijah Molloy <elijahmolloy@gmail.com>
2024-11-06 17:03:39 -0700

    Initial commit

---
+++ WeatherForecastApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherForecastApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WeatherForecastApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherForecastApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
