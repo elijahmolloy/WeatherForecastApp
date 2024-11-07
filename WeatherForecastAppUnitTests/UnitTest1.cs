9ab8153eb0e9c9fb349bac1368024f90b113fac0
Elijah Molloy <elijahmolloy@gmail.com>
2024-11-06 18:31:59 -0700

    Unit test project added

---
+++ WeatherForecastAppUnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace WeatherForecastAppUnitTests;
+
+[TestFixture]
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+}
